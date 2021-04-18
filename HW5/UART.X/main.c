673e97f9fa3eb4ef735f1f0e8ede2e28f109c529
billyen33 <billmembers33@gmail.com>
2021-04-18 15:49:44 -0500

    HW6 edits

--- HW5/UART.X/main.c
+++ HW5/UART.X/main.c
@@ -12,10 +12,10 @@
 #pragma config CP = OFF // disable code protect
 
 // DEVCFG1
-#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FNOSC = FRCPLL // use primary oscillator with pll
 #pragma config FSOSCEN = OFF // disable secondary oscillator
 #pragma config IESO = OFF // disable switching clocks
-#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config POSCMOD = OFF // high speed crystal mode
 #pragma config OSCIOFNC = OFF // disable clock output
 #pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
 #pragma config FCKSM = CSDCMD // disable clock switch and FSCM
@@ -25,9 +25,9 @@
 #pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
-#pragma config FPLLIDIV = DIV_4 // divide input clock to be in range 4-5MHz
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
 #pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
-#pragma config FPLLODIV = DIV_1 // divide clock after FPLLMUL to get 48MHz
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
 
 // DEVCFG3
 #pragma config USERID = 0 // some 16bit userid, doesn't matter what
